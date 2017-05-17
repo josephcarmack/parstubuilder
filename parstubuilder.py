41f7f1258476f38c20506318315099f7613a656c
josephcarmack <joseph.liping@gmail.com>
2017-05-17 13:07:20 -0500

    passing 5 init testswq

--- parstubuilder.py
+++ parstubuilder.py
@@ -2,9 +2,41 @@
 
 class ParametricStudy:
 
-    def __init__(self):
-        self.studyName = 'Study Name'
-        self.pathToStudy = 'path-to-study'
-        self.inputFileGen = 'input file generator executable name'
-        self.inputFileMod = 'input file modifier executable name'
-        self.parametric_info = {}
+    def __init__(self,**kwargs):
+        if len(kwargs) == 0:
+            self.studyName = 'Study Name'
+            self.pathToStudy = 'path-to-study'
+            self.inputFileGen = 'input file generator executable name'
+            self.inputFileMod = 'input file modifier executable name'
+            self.parametric_info = {}
+        else:
+            self.studyName = 'Study Name'
+            self.pathToStudy = 'path-to-study'
+            self.inputFileGen = 'input file generator executable name'
+            self.inputFileMod = 'input file modifier executable name'
+            self.parametric_info = {}
+            validKwargs = {
+                    'studyName':self.studyName,
+                    'pathToStudy':self.pathToStudy,
+                    'inputFileGen':self.inputFileGen,
+                    'inputFileMod':self.inputFileMod,
+                    'parametric_info':self.parametric_info
+                    }
+
+            # check for valid keyword arguments
+            for key in kwargs:
+                if key not in validKwargs:
+                    eflag = key
+                    print(str(eflag)+' is not a valid keyword! Valid keyword args:')
+                    for k in validKwargs:
+                        print(k)
+                    raise ValueError('invalid keyword argument')
+
+            # assign keyword args to respective class attributes
+            for key in kwargs:
+                validKwargs[key] = kwargs[key]
+            self.studyName = validKwargs['studyName']
+            self.pathToStudy = validKwargs['pathToStudy']
+            self.inputFileGen = validKwargs['inputFileGen']
+            self.inputFileMod = validKwargs['inputFileMod']
+            self.parametric_info = validKwargs['parametric_info']
--- tests/test_parstubuilder.py
+++ tests/test_parstubuilder.py
@@ -13,7 +13,7 @@ def test_no_arg_init():
     assert myStudy.inputFileMod == 'input file modifier executable name'
     assert myStudy.parametric_info == {}
 
-def test_kwargs_init():
+def test_all_kwargs_init():
     myStudy = ps(
             studyName='test name',
             pathToStudy='/path/to/study/',
@@ -26,3 +26,34 @@ def test_kwargs_init():
     assert myStudy.inputFileGen == 'mesoGen.py'
     assert myStudy.inputFileMod == 'mesoMod.py'
     assert myStudy.parametric_info == {'par1':[0,1,2],'par2':[3,4,5]}
+
+def test_some_kwargs_init():
+    myStudy = ps(
+            studyName='test name',
+            inputFileMod='mesoMod.py',
+            pathToStudy='/path/to/study/',
+            )
+    assert myStudy.studyName == 'test name'
+    assert myStudy.pathToStudy == '/path/to/study/'
+    assert myStudy.inputFileGen == 'input file generator executable name'
+    assert myStudy.inputFileMod == 'mesoMod.py'
+    assert myStudy.parametric_info == {}
+
+def test_some_kwargs_random_order_init():
+    myStudy = ps(
+            pathToStudy='/path/to/study/',
+            inputFileMod='mesoMod.py',
+            studyName='test name',
+            )
+    assert myStudy.studyName == 'test name'
+    assert myStudy.pathToStudy == '/path/to/study/'
+    assert myStudy.inputFileGen == 'input file generator executable name'
+    assert myStudy.inputFileMod == 'mesoMod.py'
+    assert myStudy.parametric_info == {}
+
+def test_invalid_kwarg_init():
+    with pytest.raises(ValueError):
+        myStudy = ps(
+                pathToStudy='/path/to/study/',
+                invalidKwarg='this is a test'
+                )
