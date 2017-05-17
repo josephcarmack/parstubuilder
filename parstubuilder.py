ddc7e694fcad7b7b0b59d5171f167c2418e155fe
josephcarmack <joseph.liping@gmail.com>
2017-05-17 11:30:43 -0500

    passing 1st init test, create 2nd init test

--- parstubuilder.py
+++ parstubuilder.py
@@ -1,4 +1,10 @@
 
 
 class ParametricStudy:
-    pass
+
+    def __init__(self):
+        self.studyName = 'Study Name'
+        self.pathToStudy = 'path-to-study'
+        self.inputFileGen = 'input file generator executable name'
+        self.inputFileMod = 'input file modifier executable name'
+        self.parametric_info = {}
--- tests/test_parstubuilder.py
+++ tests/test_parstubuilder.py
@@ -12,3 +12,17 @@ def test_no_arg_init():
     assert myStudy.inputFileGen == 'input file generator executable name'
     assert myStudy.inputFileMod == 'input file modifier executable name'
     assert myStudy.parametric_info == {}
+
+def test_kwargs_init():
+    myStudy = ps(
+            studyName='test name',
+            pathToStudy='/path/to/study/',
+            inputFileGen='mesoGen.py',
+            inputFileMod='mesoMod.py',
+            parametric_info={'par1':[0,1,2],'par2':[3,4,5]}
+            )
+    assert myStudy.studyName == 'test name'
+    assert myStudy.pathToStudy == '/path/to/study/'
+    assert myStudy.inputFileGen == 'mesoGen.py'
+    assert myStudy.inputFileMod == 'mesoMod.py'
+    assert myStudy.parametric_info == {'par1':[0,1,2],'par2':[3,4,5]}
