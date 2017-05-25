30657959c422796674ba60428e30c33709cadce4
josephcarmack <joseph.liping@gmail.com>
2017-05-25 13:58:45 -0500

    hpcExecute is running demo.py successfully on HPC

--- parstubuilder.py
+++ parstubuilder.py
@@ -5,7 +5,12 @@ import subprocess as sp
 class ParametricStudy:
 
     def __init__(self,**kwargs):
+        self.startDir = os.getcwd()
+        self.numOfParamSets = None
+        self.subDir = None
+        self.listOfSets = None
         self.buildComplete = False
+        self.allJobs = None
         if len(kwargs) == 0:
             self.studyName = None
             self.pathToStudy = None
@@ -99,21 +104,21 @@ class ParametricStudy:
 
         # ------------------------------------------------
         # create subdirectories, input files, executables,
-        # and PBS files for the parametric study (i.e. 
+        # and PBS files for the parametric study (i.e.
         # one for each unique set of parameters)
         # ------------------------------------------------
 
         # calculate the total number of unique parameter sets
-        numberOfParamSets = 1
+        self.numOfParamSets = 1
         for k in sorted(self.parametric_info):
-            numberOfParamSets *= len(self.parametric_info[k])
-        
+            self.numOfParamSets *= len(self.parametric_info[k])
+
         # make a list that holds a dictionary for each unique parameter set
         container = self.parametric_info.copy()
         for k in container:
             container[k] = 0
         self.listOfSets = []
-        for i in range(numberOfParamSets):
+        for i in range(self.numOfParamSets):
             self.listOfSets.append(container.copy())
 
         # calculate each unique parameter set
@@ -121,7 +126,7 @@ class ParametricStudy:
         for parameter in sorted(self.parametric_info):
             numParValues = len(self.parametric_info[parameter])
             val_i =0
-            for i in range(numberOfParamSets):
+            for i in range(self.numOfParamSets):
                 self.listOfSets[i][parameter] = self.parametric_info[parameter][val_i]
                 if i%skip == 0:
                     val_i += 1
@@ -140,7 +145,7 @@ class ParametricStudy:
             subDirName = '/'
             for param in s:
                 subDirName += str(param)+str(s[param])
-            pathPlusSub = self.pathToStudy+self.studyName+subDirName 
+            pathPlusSub = self.pathToStudy+self.studyName+subDirName
             self.subDir.append(pathPlusSub)
 
             os.makedirs(pathPlusSub)
@@ -192,21 +197,85 @@ class ParametricStudy:
     def hpcExecute(self,numConcJobs):
 
         # make sure build method has been called aready
+        if not self.buildComplete:
+            print('You must run the build method before running the hpcExecute method')
         assert self.buildComplete
+
+        # -----------------------
+        # check for correct input
+        # -----------------------
+
+        # convert numConcJobs to an integer if it is not already
+        numConcJobs = int(numConcJobs)
+
+        # make sure numConcJobs is greater than zero
+        if numConcJobs < 1:
+            print('0 < numConcJobs <= total number of parameter sets')
+        assert numConcJobs > 1
+
+        # make sure numConcJobs is less than the number of parameter sets
+        if not (numConcJobs <= self.numOfParamSets):
+            print('numConcJobs must be less than the total number of parameter sets.')
+            print('for example, if you have two parameters "a" and "b" and each will')
+            print('have 3 unique values, then the total number of unique (a,b) pairs')
+            print('will be equal to 9, thus yielding a total of 9 total jobs to be')
+            print('to be submitted. Therefore numConcJobs must be less than or equal')
+            print('to 9.')
+        assert numConcJobs <= self.numOfParamSets
 
         # -----------------------------------------------
         # loop over the list of unique parameter sets and
         # submit them to the HPC
         # -----------------------------------------------
 
-        self.jobIDs = []
+        jobIDs = []
+        self.allJobs = []
 
         # start the first batch of jobs to run simultaneously
         for i in range(numConcJobs):
-            # start a job
+            # change to the sub-directory to start the job
             os.chdir(self.subDir[i])
-            #jID = sp.run('qsub '+self.defaultPBSFileName,shell=True,stdout=sp.PIPE)
-            #self.jobIDs.append(jID.stdout.decode('utf-8').split('.')[0])
-            jID = '111'+str(i)
-            print(os.getcwd())
-            print('running job '+jID)
+            # build the command to submit job to the HPC
+            cmd = ['qsub',self.defaultPBSFileName]
+            # submit the job and get the job ID
+            jID = sp.check_output(cmd)
+            # store the job ID in a list
+            jobIDs.append(jID.split('.')[0])
+            # keep a running list of all job IDs
+            self.allJobs.append(jID.split('.')[0])
+
+        # start the rest of the jobs on hold until the first batch finishes
+        nextBatch = []
+        for id in jobIDs:
+            nextBatch.append(id)
+        count = 1
+        for sd in self.subDir[numConcJobs:]:
+            os.chdir(sd)
+            jobStr = ':'.join(nextBatch)
+            # build the command to submit job to the HPC
+            cmd = ['qsub','-W','depend=afterany:'+jobStr,self.defaultPBSFileName]
+            print(cmd)
+            # submit the job and get the job ID
+            jID = sp.check_output(cmd)
+            # store the job ID in a list
+            jobIDs.append(jID.split('.')[0])
+            # update running list of all job IDs
+            self.allJobs.append(jID.split('.')[0])
+            # make sure job list never grows beyond numConcJobs
+            jobIDs.pop(0)
+            # update nextBatch
+            if (count % numConcJobs) == 0:
+                for i in range(numConcJobs):
+                    nextBatch[i] = jobIDs[i]
+            count += 1
+
+        # -----------------------------------------------
+        # write job ID's to the file 'jobIDs.txt'
+        # -----------------------------------------------
+
+        # change back to the starting directory
+        os.chdir(self.startDir)
+        with open('jobIDs.txt','w') as fout:
+            for jobID in self.allJobs:
+                fout.write(jobID+'\n')
+        fout.close()
